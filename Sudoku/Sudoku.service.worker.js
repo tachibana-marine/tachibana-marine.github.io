fcf57833f843f95382d0e5a6362d2f011328e08d
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-02-10 01:22:48 +0900

    add VRAM support

--- Sudoku/Sudoku.service.worker.js
+++ Sudoku/Sudoku.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739117698|3458608549';
+const CACHE_VERSION = '1739118136|3896725903';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
