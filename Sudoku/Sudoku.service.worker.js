12ea8f4fd7b6b1ac69f925058be9597f09830356
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-02-10 01:51:12 +0900

    re:remove debug

--- Sudoku/Sudoku.service.worker.js
+++ Sudoku/Sudoku.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739119509|1025947094';
+const CACHE_VERSION = '1739119809|1326697765';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
