7070f757c7991937b762312b92b9fdb4a2656885
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-02-10 01:15:30 +0900

    Reverting

--- Sudoku/Sudoku.service.worker.js
+++ Sudoku/Sudoku.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739117460|3221163717';
+const CACHE_VERSION = '1739117698|3458608549';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
