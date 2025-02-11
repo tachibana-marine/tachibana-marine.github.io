eaae2735ee0029eb07bfd894938e903b3c006d7b
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-02-11 20:16:12 +0900

    add ios build

--- Sudoku/Sudoku.service.worker.js
+++ Sudoku/Sudoku.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739119809|1326697765';
+const CACHE_VERSION = '1739272046|409558352';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
