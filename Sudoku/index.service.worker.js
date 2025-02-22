f1c95c1585fa5515da47a1e060a0fd4bae11d912
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-02-22 18:51:39 +0900

    cors

--- Sudoku/index.service.worker.js
+++ Sudoku/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1740215966|3314981668';
+const CACHE_VERSION = '1740217871|5220460411';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
