4802a44bf1583828f89339ee491ee631363eeaf9
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-03-07 02:20:57 +0900

    remove audio

--- Sudoku/index.service.worker.js
+++ Sudoku/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741279992|12894500474';
+const CACHE_VERSION = '1741281609|14511871640';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
