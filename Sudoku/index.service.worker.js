c8160dc6d05a40d4ae932c1dbace3736f30ad58e
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-03-08 22:31:54 +0900

    add button

--- Sudoku/index.service.worker.js
+++ Sudoku/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741439149|3460767';
+const CACHE_VERSION = '1741440666|3538301';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
