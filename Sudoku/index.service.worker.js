c3ff2c84551f9398cadb58ac7d571fcb0b785c3a
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-03-11 00:57:14 +0900

    Sudoku alpha 0.3.2

--- Sudoku/index.service.worker.js
+++ Sudoku/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741531465|3681720';
+const CACHE_VERSION = '1741622109|7023214';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
