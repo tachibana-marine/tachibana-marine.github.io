db28bd3aab3787e01bddd6f0fa06d8c350933cf8
tachibana-marine <d.ageha.sekaijyu1041@gmail.com>
2025-03-16 22:28:58 +0900

    Sudoku web-4.2

--- Sudoku/index.service.worker.js
+++ Sudoku/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742130387|3536089';
+const CACHE_VERSION = '1742131728|5216909';
 /** @type {string} */
 const CACHE_PREFIX = 'Sudoku-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
